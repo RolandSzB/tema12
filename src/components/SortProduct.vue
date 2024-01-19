cc4cf49a7458015836bdc28cd17cccf1484296de
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-19 09:20:48 +0200

    arrows direction changed

--- src/components/SortProduct.vue
+++ src/components/SortProduct.vue
@@ -13,32 +13,32 @@ defineProps({
       @click="$emit('sortTopRated')"
     >
       Top rated
-      <i v-if="sortSens === 'asc'" class="bi bi-arrow-up"></i>
-      <i v-else class="bi bi-arrow-down"></i>
+      <i v-if="sortSens === 'asc'" class="bi bi-arrow-down"></i>
+      <i v-else class="bi bi-arrow-up"></i>
     </button>
     <button
       class="m-3 px-3 py-1 bg-slate-500 rounded-xl"
       @click="$emit('sortPopular')"
     >
       Popular
-      <i v-if="sortSens === 'asc'" class="bi bi-arrow-up"></i>
-      <i v-else class="bi bi-arrow-down"></i>
+      <i v-if="sortSens === 'asc'" class="bi bi-arrow-down"></i>
+      <i v-else class="bi bi-arrow-up"></i>
     </button>
     <button
       class="m-3 px-3 py-1 bg-slate-500 rounded-xl"
       @click="$emit('sortPrice')"
     >
       Price
-      <i v-if="sortSens === 'asc'" class="bi bi-arrow-up"></i>
-      <i v-else class="bi bi-arrow-down"></i>
+      <i v-if="sortSens === 'asc'" class="bi bi-arrow-down"></i>
+      <i v-else class="bi bi-arrow-up"></i>
     </button>
     <button
       class="m-3 px-3 py-1 bg-slate-500 rounded-xl"
       @click="$emit('sortNewest')"
     >
       Newest
-      <i v-if="sortSens === 'asc'" class="bi bi-arrow-up"></i>
-      <i v-else class="bi bi-arrow-down"></i>
+      <i v-if="sortSens === 'asc'" class="bi bi-arrow-down"></i>
+      <i v-else class="bi bi-arrow-up"></i>
     </button>
   </div>
 </template>
